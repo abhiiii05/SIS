ba000269b06f940568fa4da3e0d47886fa1197d1
abhiiii.iiii <abhijithviju2005@gmail.com>
2024-05-17 11:36:43 +0400

    LOGIN FINAL CSS DONE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import SignInWithGoogle from './SignInWithGoogle';
 import './index.css'
+import schoolLogo from  './trial-removebg-preview.png'
 
 
 function App() {
@@ -12,19 +13,26 @@ function App() {
         window.location.replace('.src/welcome.js')
      }
     return (
-        <div className="min-h-screen bg-[#183957]  flex flex-col justify-center items-center">
+        <div className="min-h-screen h-screen bg-[#183957] flex flex-col items-center justify-center relative">
+            <div className="absolute -top-44 -left-44 circle-top-left"></div>
+            <div className="absolute -bottom-44 -right-44 circle-bottom-right"></div>
             <GoogleOAuthProvider clientId="50247589356-79m9e84j9naggbu4dhsmnu1kv5an8khj.apps.googleusercontent.com">
-                <div className="App ">
-                    <h1 className="text-white font-bold font-jomolhari mb-8 fixed top-14 left-0 right-0 bg-[#183957] py-4 text-center text-4xl">SHARJAH
-                        INDIAN SCHOOL JUWAIZA</h1>
-                    {isLoggedin ? (<h1></h1>) :
-                         (<SignInWithGoogle onSuccessfulSignIn={successfulSignIn}/>)}
+                <div className="App">
+                    <h1 className="text-white font-bold font-jomolhari mb-8 bg-[#183957] py-4 text-center text-4xl">
+                        SHARJAH INDIAN SCHOOL JUWAIZA
+                    </h1>
+                    <img src={schoolLogo} alt="School Logo" className="w-32 translate-x-72 mb-8"/>
+                    {isLoggedin ? (
+                        <h1></h1>
+                    ) : (
+                        <SignInWithGoogle onSuccessfulSignIn={successfulSignIn}/>
+                    )}
                 </div>
-</GoogleOAuthProvider>
-</div>
+            </GoogleOAuthProvider>
+        </div>
 
-)
-    ;
+    )
+        ;
 }
 
 
--- src/SignInWithGoogle.jsx
+++ src/SignInWithGoogle.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import schoolLogo from  './trial-removebg-preview.png'
 
 const handleGoogleSignIn = (response) => {
     const { profileObj } = response;
@@ -11,39 +12,35 @@ const SignInWithGoogle = () => {
     const clientId = '50247589356-79m9e84j9naggbu4dhsmnu1kv5an8khj.apps.googleusercontent.com'; // Replace with your client ID
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="bg-[#A5BCC9] rounded-lg shadow-md p-8 w-80 h-72 max-w-md">
-                <div className="flex flex-col items-center">
-                    <div className="bg-[#A5BCC9] rounded-full p-11 mb-8">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="w-8 h-8 text-gray-600"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </div>
-                    <GoogleLogin
-                        clientId={clientId}
-                        onSuccess={handleGoogleSignIn}
-                        render={(renderProps) => (
-                            <button
-                                className="bg-white text-gray-800 font-bold py-8 px-12 rounded shadow-md hover:bg-gray-100 transition-colors duration-300 w-full text-2xl"
-                                onClick={renderProps.onClick}
-                                disabled={renderProps.disabled}
-                            >
-                                Sign in with Google
-                            </button>
-                        )}
-                        cookiePolicy={'single_host_origin'} // Recommended for security
+        <div className="bg-[#A5BCC9] rounded-lg shadow-md p-8 w-80 max-w-md flex flex-col items-center translate-x-48">
+            <div className="bg-[#A5BCC9] rounded-full p-11 mb-8">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-8 h-8 text-gray-600"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                        clipRule="evenodd"
                     />
-                </div>
+                </svg>
             </div>
+            <GoogleLogin
+                clientId={clientId}
+                onSuccess={handleGoogleSignIn}
+                render={(renderProps) => (
+                    <button
+                        className="bg-white text-gray-800 font-bold py-8 px-12 rounded shadow-md hover:bg-gray-100 transition-colors duration-300 w-full text-2xl"
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                    >
+                        Sign in with Google
+                    </button>
+                )}
+                cookiePolicy={'single_host_origin'} // Recommended for security
+            />
         </div>
     );
 };
